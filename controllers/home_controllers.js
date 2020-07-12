384b2482686497fc69a8f52d40166fbf3c6e03cc
sarthak virmani <sarthak@sarthaks-MacBook-Air.local>
2020-07-13 00:44:09 +0530

    add a controler and router from home

---
+++ controllers/home_controllers.js
@@ -0,0 +1,3 @@
+module.exports.home =function(req,res){ //home is the actionName here where we are crearting a controller
+    return res.end('<h1> Express is up in codeial</h1?');
+}
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 9000;
-app.listen(port,function(){
-    if(err){
+
+// use express router(middle ware )
+app.use('/',require('./routes/index'));
+
+app.listen(port,function(err){
+    if (err){
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running in the port: ${port}`);
